7252b99dae306f23c62c530c45e4d540a782c0d1
dmaust <david@dmaust.net>
2014-04-25 21:56:25 -0700

    Complete implementation of cacheMatrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A matrix wrapper which allows caching of the inverse of a matrix.
+## 
+## Example usage: 
+## 
+## > mymat <- makeCacheMatrix(matrix(c(1, 2, 3, 4), 2, 2))
+## > myinv <- cacheSolve(mymat)
+## > myinv
+## [,1] [,2]
+## [1,]   -2  1.5
+## [2,]    1 -0.5
 
-## Write a short comment describing this function
+## Creates an object which holds a matrix together with its cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(arg) inv <<- arg
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Returns the inverse of a matrix held in the cacheMatrix object.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
 }
